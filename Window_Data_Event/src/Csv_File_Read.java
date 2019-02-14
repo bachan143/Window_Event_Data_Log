1d8c9a757d866854d920bafc325a53702dec57b3
bachan143 <bachanghatuary@gmail.com>
2019-02-14 13:36:14 +0530

    first commit

---
+++ Window_Data_Event/src/Csv_File_Read.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+public class Csv_File_Read 
+{
+
+
+	public static void main(String[] args) throws IOException
+	{
+		String filename="f://window/data1.csv";
+		FileReader file=new FileReader(filename);
+	    System.out.println("input the event id ");
+	    int event;
+		Scanner sc=new Scanner(System.in);
+		event=sc.nextInt();
+		try {
+			int i;
+			
+			
+			while((i=file.read())!=-1)
+			{
+				
+			
+				
+				System.out.print((char)i);
+			}
+			System.out.println();
+		
+		} 
+		catch (FileNotFoundException e) 
+		{
+		
+			e.printStackTrace();
+		}
+		
+	}
+
+}
